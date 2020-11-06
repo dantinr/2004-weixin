da9e3478b7b6ae5fbb93fa3f7d47313e61c58f41
dante <dantinr@gmail.com>
2020-11-06 14:45:33 +0800

    处理事件推送

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -28,6 +28,27 @@ class WxController extends Controller
         }
     }
 
+    public function wxEvent()
+    {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = env('WX_TOKEN');
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){            //验证通过
+            echo "";
+
+
+        }else{
+            echo "";
+        }
+    }
+
 
     /**
      * 获取access_token
